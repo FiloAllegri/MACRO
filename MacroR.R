e529aa9056b4d1c13dcb2626c7869b9eba7f4602
‘FiloAllegri‘ <41872@student.hhs.se>
2021-05-06 18:13:46 +0200

    R file and merging data

---
+++ MacroR.R
@@ -0,0 +1,18 @@
+library("plyr")  # load plyr
+library("tidyverse") # load tidyverse
+library("rio") # load rio
+library("magrittr") # load magrittr
+library("ggthemes") # load ggthemes
+library("lmtest") # load lmtest
+library("sandwich") # load sandwich
+library("broom") # load broom
+
+
+TaxRevenue <- import("TaxRevenue.csv")
+TaxWedge <- import("TaxWedge.csv")
+
+
+TaxRevenueC <- TaxRevenue[ -c(2,3,4,5,7,8) ]
+TaxWedgeC <- TaxWedge[ -c(2,3,4,5,7,8,9) ]
+
+Tax <- merge(x = TaxWedgeC, y = TaxRevenueC, by = c("LOCATION", "TIME"), all=TRUE)
